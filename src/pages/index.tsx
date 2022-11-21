8ea33015490789f209a6a321ffff41fc14b01476
Felipe Molinos <felipemolinos01@gmail.com>
2022-11-21 20:50:37 -0300

    feat: added basic home page structure and css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,73 @@
 import Head from "next/head";
 import React from "react";
 
+import {
+  InfosContainer,
+  InfosSection,
+  JobsInfos,
+  PageContainer,
+  PageHeader,
+  PageHeadingBackground,
+  ProfileSection,
+  WorktimeLeftContainer,
+  Wrapper,
+} from "./styles";
+
 export default function Home() {
   return (
-    <Head>
-      <title>Home | JobsCalc</title>
-    </Head>
+    <Wrapper>
+      <Head>
+        <title>Home | JobsCalc</title>
+      </Head>
+
+      <PageHeadingBackground />
+
+      <PageContainer>
+        <PageHeader>
+          <img src="/images/logo.svg" alt="JobsCalc Logo" />
+
+          <WorktimeLeftContainer>
+            <img
+              src="/images/alert-icon.svg"
+              alt="Amount of work time left (in hours)"
+            />
+            <p>Você tem 2 horas livres no seu dia</p>
+          </WorktimeLeftContainer>
+
+          <ProfileSection>
+            <div>
+              <p>Jaqueline</p>
+              <p>Ver perfil</p>
+            </div>
+
+            <img src="/images/profile-placeholder.svg" alt="Profile Picture" />
+          </ProfileSection>
+        </PageHeader>
+
+        <InfosSection>
+          <InfosContainer>
+            <JobsInfos>
+              <strong>12</strong>
+              <p>Projetos ao total</p>
+            </JobsInfos>
+            <JobsInfos hasMargin>
+              <strong>7</strong>
+              <p>Em andamento</p>
+            </JobsInfos>
+            <JobsInfos hasMargin>
+              <strong>4</strong>
+              <p>Encerrados</p>
+            </JobsInfos>
+          </InfosContainer>
+
+          <button>
+            <div>
+              <img src="/images/plus-icon.svg" alt="Add job plus icon" />
+            </div>
+            <p>ADICIONAR NOVO JOB</p>
+          </button>
+        </InfosSection>
+      </PageContainer>
+    </Wrapper>
   );
 }
---
+++ src/pages/styles.ts
@@ -0,0 +1,131 @@
+import styled from "styled-components";
+
+interface JobsInfoProps {
+  hasMargin: boolean | false;
+}
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const PageHeadingBackground = styled.div`
+  width: 100%;
+  height: 280px;
+
+  background: #41414c;
+  position: absolute;
+`;
+
+export const PageContainer = styled.div`
+  width: 1440px;
+
+  z-index: 9999;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const PageHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+
+  padding: 1.5rem 0;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: #4f4f5b;
+`;
+
+export const WorktimeLeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  p {
+    color: #fcfdff;
+    margin-left: 10px;
+  }
+`;
+
+export const ProfileSection = styled.aside`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  div p:first-child {
+    font-size: 20px;
+    font-weight: 600;
+    font-family: "IBM Plex Sans", sans-serif;
+    color: #fcfdff;
+  }
+
+  div p:last-child {
+    font-size: 14px;
+    font-family: "IBM Plex Sans", sans-serif;
+    color: #bfbfcc;
+    text-align: right;
+  }
+
+  img {
+    margin-left: 20px;
+  }
+`;
+
+export const InfosSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+
+  padding: 2rem 0;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 12px 32px 12px 12px;
+
+    width: 260px;
+
+    background: #f1972c;
+
+    font-size: 14px;
+    font-weight: 700;
+    color: #fcfdff;
+
+    border-radius: 5px;
+
+    img {
+      padding: 4px;
+    }
+  }
+`;
+
+export const InfosContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const JobsInfos = styled.div`
+  margin-left: ${(props) => (props.hasMargin ? "2rem" : "0")};
+
+  strong {
+    font-size: 24px;
+    color: #fcfdff;
+  }
+
+  p {
+    font-size: 16px;
+    color: #fcfdff;
+  }
+`;
